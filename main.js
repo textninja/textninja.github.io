bbff7a08af454b37796818477c48a2a25341381b
Joe Taylor <joe@textninja.net>
2021-12-11 22:06:42 -0800

    Moved inline script to external

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 import * as d3 from "https://cdn.skypack.dev/d3@7";
 
+function hideWhenFinished() {
+    document.querySelector("#dimmer").style.display = "none";
+}
+
+document.querySelector("header input[type=search]").addEventListener("focus", () => {
+    document.querySelector("#dimmer").removeEventListener("transitionend", hideWhenFinished);
+    document.querySelector("#dimmer").style.display = "block";
+    setTimeout(() => document.querySelector("#dimmer").className = "active", 0);
+});
+
+document.querySelector("header input[type=search]").addEventListener("blur", () => {
+    document.querySelector("#dimmer").addEventListener("transitionend", hideWhenFinished);    
+    document.querySelector("#dimmer").className = "";
+});
+
 function setArtSize() {
     const headerSvg = document.querySelector("header svg");
     const svgContainer = headerSvg.parentNode;
